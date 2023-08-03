3cdc499d6e629ccdf6a4564f08bf6ea227b44dc7
sargesmith <shittuisrael004@gmail.com>
2023-08-03 23:49:15 +0100

    SafeMathTester.sol

---
+++ SafeMathTester.sol
@@ -0,0 +1,30 @@
+// SPDX-Licence-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+// Solidity Versions 0.7 & 0.6 data types are unchecked, they can wrap which can cause overflow and underflow
+
+contract SafeMathTester{
+    uint8 public bigNumber = 255; 
+
+    function add() public{
+        bigNumber = bigNumber + 1;
+    }
+}
+
+////////////////////
+/// SAME AS ////////
+////////////////////
+
+// pragma solidity ^0.8.0;
+
+// // Solidity Versions 0.8 solved the unchecked issue, they would revert an error incase of overflow or underflow
+
+//  contract SafeMathTester{
+//     uint8 public bigNumber = 255; 
+
+//     function add() public{
+//        unchecked { bigNumber = bigNumber + 1};
+//        // with 'unchecked' keyword, this code will wrap, uint8 (255 + 1) == 0
+//     }
+//  }
